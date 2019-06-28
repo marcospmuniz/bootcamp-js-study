70dcf9a41b25107e729a45cb8a178a2051c1f971
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-06-27 22:15:13 -0300

    inicio do beckend do gobarber

---
+++ modulo02/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// exporta só o atributo 'server' da classe
+// ATENÇÃO!
+// Só estou usando 'export default' no lugar de 'module.exports = '
+// porque  estou usando a lib sucrase nesse projeto
+export default new App().server;
