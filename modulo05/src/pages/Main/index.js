c3739da3c21f00bb4ecb22de5e5a61a28f3cab74
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-08-05 22:33:40 -0300

    add modulo05

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main() {
+  return (
+    <Title>
+      Main
+      <small>small</small>
+    </Title>
+  );
+}
+
+export default Main;
