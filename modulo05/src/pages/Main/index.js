c250a757eb3d0bd80477a70ebc2725bd633ef485
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-08-07 23:20:18 -0300

    feature: add listagem dos repositorios e armazenamento no localStorage

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -4,7 +4,7 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
   state = {
@@ -12,6 +12,25 @@ class Main extends Component {
     repositories: [],
     loading: false,
   };
+
+  // carregar os dados do LocalStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      // convert de string para objeto json
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // salvar os dados do LocalStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
@@ -38,7 +57,7 @@ class Main extends Component {
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, loading, repositories } = this.state;
 
     return (
       <Container>
@@ -63,6 +82,15 @@ class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
